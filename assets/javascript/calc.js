033418c7725da1a1d7eb3265dad3aa0b0dcaa74a
mkdoh3 <mkdoherty3@GMAIL.COM>
2017-09-16 10:12:27 -0500

    js updated

--- assets/javascript/calc.js
+++ assets/javascript/calc.js
@@ -3,15 +3,14 @@
 
      var num1 = "";
      var nums = [];
-     //operator initiated with placeholder so that an operator click is ignored if it comes before a number click
-     var operator = "#";
+     var operator = "";
      var results = "";
 
 
      $(".number").on("click", function () {
          var press = $(this).attr("value");
         //grab html value, make operator a valid btn
-         operator = "";
+         operator = "#";
          num1 += press;
          //update num1 in html
          $("#result").html(num1);
@@ -26,8 +25,7 @@
              nums.push(num1);
          }
          //grab operator value, push it to nums array and update html. reset num1 for next input
-         //if (!operator) also prevents sequential operator presses
-         if (!operator) { 
+         if (operator) { 
              operator = $(this).attr("value");
              nums.push(operator);
              $("#result").html(operator);
@@ -39,8 +37,7 @@
      //push last num1 and eval the string in nums array
      $(".equal").on("click", function () {
          nums.push(num1);
-         //setting operator as empty allows for operator on the stored results
-         operator = "";
+         operator = "#";
          num1 = "";
 
          //eval doesnt work with '^' so we grab the values on either side of a "^" index, eval them with math.pow,
@@ -65,7 +62,7 @@
      $(".clear").on("click", function () {
          num1 = "";
          nums = [];
-         operator = "#";
+         operator = "";
          results = "";
          $("#result").html("");
      });
